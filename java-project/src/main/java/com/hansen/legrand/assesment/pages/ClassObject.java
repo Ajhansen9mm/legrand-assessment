121024047a36aa4346b0e887ffd2d6e2a52e6165
ajhan <ajhansen.9mm@gmail.com>
2022-10-22 14:43:04 -0500

    next commit

--- java-project/src/main/java/com/hansen/legrand/assesment/pages/ClassObject.java
+++ java-project/src/main/java/com/hansen/legrand/assesment/pages/ClassObject.java
@@ -3,6 +3,11 @@ package com.hansen.legrand.assesment.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ClassObject {
     private WebDriver driver;
@@ -18,13 +23,16 @@ public class ClassObject {
     private By userLogin = By.xpath("//*[@id=\"loginusername\"]");
     private By passwordLogin = By.xpath("//*[@id=\"loginpassword\"]");
     private By enterLogIn = By.xpath("//*[@id=\"logInModal\"]/div/div/div[3]/button[2]");
-    private By signOut = By.xpath("//*[@id=\"logout2\"]");
+    private By signOut = By.id("logout2");
     private By signUpCancel = By.xpath("//*[@id=\"signInModal\"]/div/div/div[3]/button[1]");
     private By itemOne = By.xpath("//*[@id=\"tbodyid\"]/div[1]/div/div/h4");
     private By addCart = By.xpath("//*[@id=\"tbodyid\"]/div[2]/div/a");
-    private By cart = By.xpath("//*[@id=\"cartur\"]");
+    private By cart = By.cssSelector("#navbarExample > ul > li:nth-child(4) > a");
     private By deleteCart = By.xpath("//*[@id=\"tbodyid\"]/tr[1]/td[4]/a");
     private By deleteCartTwo = By.xpath("//*[@id=\"tbodyid\"]/tr[2]/td[4]/a");
+    private By deleteBtnVisible = By.xpath("//*[@id=\"tbodyid\"]/tr/td[4]/a");
+    private By cartNumber = By.id("totalp");
+
 
 
 
@@ -43,52 +51,72 @@ public class ClassObject {
 //    }
 
     public void signUp() {
-        this.driver.findElement(signUp).click();
+        this.findElement(signUp).click();
     }
 
 //    public void clickSignUp(){
 //        this.driver.findElement(signUp).click();
 //    }
     public void typePassword() {
-        this.driver.findElement(passWord).sendKeys("Password");
+        this.findElement(passWord).sendKeys("Password");
     }
     public void typeUsername() {
-        this.driver.findElement(userName).sendKeys("Ron_Burgundy");
+        this.findElement(userName).sendKeys("Ron_Burgundy");
     }
     public void enterAccount() {
-        this.driver.findElement(enterSignUp).click();
+        this.findElement(enterSignUp).click();
     }
     public void logInAcc() {
-        this.driver.findElement(logIn).click();
+        this.findElement(logIn).click();
     }
     public void enterUsername() {
-        this.driver.findElement(userLogin).sendKeys("Ron_Burgundy");
+        this.findElement(userLogin).sendKeys("Ron_Burgundy");
     }
     public void enterPassword() {
-        this.driver.findElement(passwordLogin).sendKeys("Password");
+        this.findElement(passwordLogin).sendKeys("Password");
     }
     public void signIn() {
-        this.driver.findElement(enterLogIn).click();
+        this.findElement(enterLogIn).click();
     }
     public void signOut() {
-        this.driver.findElement(signOut).click();
+        this.findElement(signOut).click();
     }
     public void signUpCancel() {
-        this.driver.findElement(signUpCancel).click();
+        this.findElement(signUpCancel).click();
     }
     public void addCartA() {
-        this.driver.findElement(addCart).click();
+        this.findElement(addCart).click();
     }
     public void clickCart() {
-        this.driver.findElement(cart).click();
+        this.findElement(cart).click();
     }
     public void deleteCartA() {
-        this.driver.findElement(deleteCart).click();
+        this.findElement(deleteCart).click();
     }
     public void deleteCartB() {
-        this.driver.findElement(deleteCartTwo).click();
+        this.findElement(deleteCartTwo).click();
     }
     public void firstItem() {
-        this.driver.findElement(itemOne).click();
+        this.findElement(itemOne).click();
+    }
+//    public String cartAmount() {
+//        return this.driver.findElement(xxxx).getText();
+
+//    }
+    public boolean logoutButtonIsDisplayed() {
+        return this.findElement(signOut).isDisplayed();
+    }
+
+    protected WebElement findElement(By by){
+        WebDriverWait wait = new WebDriverWait(this.driver, Duration.ofMillis(7000));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        return this.driver.findElement(by);
+    }
+    public String cartAmount() {
+        return this.findElement(cartNumber).getText();
+
+    }
+    public boolean delBtnIsVisible() {
+        return this.findElement(deleteBtnVisible).isDisplayed();
     }
 }
--- java-project/src/test/java/com/hansen/legrand/assesment/AppTest.java
+++ java-project/src/test/java/com/hansen/legrand/assesment/AppTest.java
@@ -1,7 +1,5 @@
 package com.hansen.legrand.assesment;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import com.hansen.legrand.assesment.pages.ClassObject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -11,6 +9,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 /**
@@ -57,43 +59,52 @@ public class AppTest {
 
 
     @Test
-    public void test1() {
+//    Create account
+    public void test1() throws InterruptedException {
         ClassObject page = new ClassObject(driver);
         driver.get("https://www.demoblaze.com/");
         page.signUp();
+        Thread.sleep(5000);
         page.typeUsername();
         page.typePassword();
         page.enterAccount();
+        Thread.sleep(4000);
+        assertTrue(page.logoutButtonIsDisplayed());
 
 
     }
 
     @Test
-    public void test2() {
+//    Log in and password valid information and log out
+    public void test2() throws InterruptedException {
         ClassObject page = new ClassObject(driver);
         driver.get("https://www.demoblaze.com/");
         page.logInAcc();
         page.enterUsername();
         page.enterPassword();
         page.signIn();
+        assertTrue(page.logoutButtonIsDisplayed());
         page.signOut();
 
 
     }
 
     @Test
-    public void test3() {
+//    Add 2 items in the shopping cart
+    public void test3() throws InterruptedException {
         ClassObject page = new ClassObject(driver);
         driver.get("https://www.demoblaze.com/");
         page.firstItem();
         page.addCartA();
         page.addCartA();
         page.clickCart();
+//        assertEquals("720", page.cartAmount());
 
 
     }
 
     @Test
+//    Create account Enter all the details in the page (First Name/ Last Name etc). Click on cancel
     public void test4() {
         ClassObject page = new ClassObject(driver);
         driver.get("https://www.demoblaze.com/");
@@ -101,19 +112,57 @@ public class AppTest {
         page.typeUsername();
         page.typePassword();
         page.signUpCancel();
+
     }
 
     @Test
-    public void test5() {
+//    Delete 2 items from the shopping cart
+    public void test5() throws InterruptedException {
         ClassObject page = new ClassObject(driver);
         driver.get("https://www.demoblaze.com/");
         page.firstItem();
         page.addCartA();
         page.addCartA();
+        Thread.sleep(3000);
         page.clickCart();
+        Thread.sleep(3000);
+        page.deleteCartA();
+        Thread.sleep(3000);
+        page.deleteCartB();
+        assertFalse(page.delBtnIsVisible());
+
 
 
     }
+    @Test
+    public void test6() throws InterruptedException {
+        ClassObject page = new ClassObject(driver);
+        driver.get("https://www.demoblaze.com/");
+        page.firstItem();
+        page.addCartA();
+        page.addCartA();
+        Thread.sleep(5000);
+        page.clickCart();
+        assertEquals("720", page.cartAmount());
+
+
+    }
+    @Test
+//    Add2/delete 1 item in the shopping cart instead of 2.
+    public void test7() throws InterruptedException {
+        ClassObject page = new ClassObject(driver);
+        driver.get("https://www.demoblaze.com/");
+        page.firstItem();
+        page.addCartA();
+        page.addCartA();
+        Thread.sleep(5000);
+        page.clickCart();
+        page.deleteCartA();
+        assertEquals(360, page.cartAmount());
+
+
+    }
+
 }
 
 
